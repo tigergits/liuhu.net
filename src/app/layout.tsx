a40521011326e1595709fcbb4e2647cc8fcfa513
Tiger <tiger.hu.liu@gmail.com>
2026-03-18 19:27:53 +0800

    fix 标题有重复内容

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
   title: {
     default: '程序员刘虎的故事 - 25+年全栈工程师的个人网站',
-    template: '%s | 程序员刘虎的故事',
+    template: '%s',
   },
   description: '刘虎，25+年开发经验的全栈工程师。这里记录了一个程序员的随笔、故事、日记、游记和技术文摘。',
   robots: {
